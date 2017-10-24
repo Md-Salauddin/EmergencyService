e124ad5c0008ef26db3e0edc039843838bcf086c
MohammadSk <salauddin.csese@gmail.com>
2017-10-24 11:10:51 +0600

    setup toolbar

--- app/src/main/java/com/example/mohammadsk/emergencyservice/MainActivity.java
+++ app/src/main/java/com/example/mohammadsk/emergencyservice/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.mohammadsk.emergencyservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
     }
 }
